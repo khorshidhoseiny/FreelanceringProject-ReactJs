d496f7ccf1147630679b0a6cc11f6a1ddffe3f41
khorshid <www.khorshid11@gmail.com>
2025-07-20 10:08:40 +0330

    change home page styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { Link, useNavigate } from "react-router-dom";
 import useUser from "../features/authentication/useUser";
 import useLogout from "../features/authentication/useLogout";
 import Loading from "../ui/Loading";
+import RainbowGeradient from "../ui/RainbowGeradient";
 
 function Home() {
   const { user, isLoading } = useUser();
@@ -9,10 +10,10 @@ function Home() {
   const { logout } = useLogout();
 
   console.log(user?.status, user?.role);
-  const roleTranslator = (role = "") => {
-    if (role === "ADMIN") return "ادمین";
-    if (role === "FREELANCER") return "فریلنسر";
-    if (role === "OWNER") return "کارفرما";
+  const rolesTranslator = {
+    ADMIN: "ادمین",
+    FREELANCER: "فریلنسر",
+    OWNER: "کارفرما",
   };
 
   const navigatorHandler = (role = "") => {
@@ -37,55 +38,67 @@ function Home() {
   ];
   return (
     <div className="h-full bg-secondary-0">
-      <div className="container flex-col flex min-h-screen items-center justify-center relative xl:max-w-screen-xl">
-        <h1 className="text-2xl md:text-3xl  justify-center flex  text-secondary-700 font-semibold">
-          <span className="bg-gradient-to-r from-blue-500  to-fuchsia-400 inline-block text-transparent bg-clip-text">
-            مرکوری&nbsp;
-          </span>
-          اپلیکیشنی پیشرفته برای فریلنسرینگ
-        </h1>
-        {isLoading && <Loading />}
-        {!user && (
-          <Link to={"/auth"}>
-            <button
-              className="text-secondary-100 bg-gradient-to-r
-         from-blue-500 to-fuchsia-400 
+      <div className="relative overflow-hidden">
+        <div className="header-gradient"></div>
+        <main className="relative z-10">
+          <div className="container flex-col flex min-h-screen items-center justify-center relative xl:max-w-screen-xl">
+            <h1 className="text-2xl fade-in md:text-3xl  justify-center flex  text-secondary-700 font-semibold">
+              <span className="bg-gradient-to-r from-blue-500  to-fuchsia-400 inline-block text-transparent bg-clip-text">
+                مرکوری&nbsp;
+              </span>
+              اپلیکیشنی پیشرفته برای فریلنسرینگ
+            </h1>
+            {isLoading && <Loading />}
+            {!user && (
+              <Link to={"/auth"}>
+                <button
+                  className="text-secondary-100 mt-7 bg-gradient-to-r
+         from-blue-500/60 to-fuchsia-400/60 transition-all duration-500 ease-in-out hover:brightness-110
          p-2 px-3 rounded-xl"
+                >
+                  ورود یا ثبت نام
+                </button>
+              </Link>
+            )}
+            <div
+              className={`flex  justify-center ${
+                !user && "hidden"
+              } items-center mt-9 gap-x-4`}
             >
-              ورود یا ثبت نام
-            </button>
-          </Link>
-        )}
-        <div className="flex justify-center mt-9 gap-x-4">
-          <h1 className="flex text-secondary-700">
-            وضعیت شما :&nbsp;
-            <p className={`${statusStyle[user?.status]?.className}`}>
-              {statusStyle[user?.status]?.label}
-            </p>
-          </h1>
-          <h1 className="flex text-secondary-700">
-            نقش شما:&nbsp; <p>{roleTranslator(user?.role)}</p>
-          </h1>
-        </div>
-        <div className="flex mt-10 justify-center items-center gap-6">
-          {user?.status === 1 ? (
-            <p>پروفایل شما در انتظار تایید مدیر است لطفا صبور باشید ...</p>
-          ) : user?.status === 2 ? (
-            <>
-              <button
-                className="gradient__effect btn p-3 text-secondary-700"
-                onClick={() => navigatorHandler(user?.role)}
-              >
-                رفتن به صفحه دشبورد
-              </button>
-              <button className="btn p-3 btn--outline" onClick={logout}>
-                خروج از حساب کاربری
-              </button>
-            </>
-          ) : (
-            ""
-          )}
-        </div>
+              <h1 className="flex text-secondary-700">
+                وضعیت شما :&nbsp;
+                <p className={`${statusStyle[user?.status]?.className}`}>
+                  {statusStyle[user?.status]?.label}
+                </p>
+              </h1>
+              <h1 className="flex justify-center items-center text-secondary-700">
+                نقش شما:&nbsp;{" "}
+                <p className="bg-primary-400 rounded-full p-2">
+                  {rolesTranslator[user?.role]}
+                </p>
+              </h1>
+            </div>
+            <div className="flex mt-10 justify-center items-center gap-6">
+              {user?.status === 1 ? (
+                <p>پروفایل شما در انتظار تایید مدیر است لطفا صبور باشید ...</p>
+              ) : user?.status === 2 ? (
+                <>
+                  <button
+                    className="gradient__effect btn p-3 text-secondary-0"
+                    onClick={() => navigatorHandler(user?.role)}
+                  >
+                    رفتن به صفحه دشبورد
+                  </button>
+                  <button className="btn p-3 btn--outline" onClick={logout}>
+                    خروج از حساب کاربری
+                  </button>
+                </>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
+        </main>
       </div>
     </div>
   );
---
+++ src/ui/RainbowGeradient.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function RainbowGeradient() {
+  return (
+    <div className="absolute w-full h-[300px] flex justify-end overflow-hidden">
+      <div className="relative w-60 h-40 rainbow-bar"></div>
+    </div>
+  );
+}
+
+export default RainbowGeradient;
